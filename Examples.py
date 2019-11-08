e71ae951892041d0048867b77c77379bb2f20b7a
Evgeniy <evgenalbah@gmail.com>
2019-11-09 00:39:49 +0300

    123

---
+++ Examples.py
@@ -0,0 +1,9 @@
+# Source: https://www.youtube.com/watch?v=x_2QpLcRdeY
+# Список,выдергивание символов из списка
+# Счет символовиз списка lis идёт по порядку и начинается с 0
+lis = [21, 22, 23, 24, 25, 26, 's', ['s', 'i', '!']]
+print(lis[1])
+
+# Ключи
+dictionary = dict.fromkeys (['a', 'b', 'c'], 1)
+print (dictionary)
